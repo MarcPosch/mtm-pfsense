0a7b40d9fe3049448b506cc50e7256a3f770d1f5
Marc Posch <MarcPosch@users.noreply.github.com>
2025-01-24 22:42:40 +0100

    Syntax fixups

--- src/usr/local/www/services_radvd.php
+++ src/usr/local/www/services_radvd.php
@@ -499,10 +499,10 @@ $numrows = count($pconfig['routes']) - 1;
 $route_priority_modes = $priority_modes;
 array_unshift($route_priority_modes, '');
 foreach ($pconfig['routes'] as $route) {
-	$route_address_name = "route_address" . $route_counter;
-	$route_bits_name = "route_bits" . $route_counter;
-	$route_priority_name = "route_priority" . $route_counter;
-	list($address, $mask) = explode("/", $route['destination']);
+	$route_address_name = 'route_address' . $route_counter;
+	$route_bits_name = 'route_bits' . $route_counter;
+	$route_priority_name = 'route_priority' . $route_counter;
+	[$address, $mask] = explode('/', $route['destination']);
 	$priority = $route['priority'];
 	$group = new Form_Group($route_counter == 0 ? 'Routes':'');
 	$group->add(new Form_IpAddress(
